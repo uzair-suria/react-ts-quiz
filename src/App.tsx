6eb9e0a4f74ddb77570c241241bed1d4865dafbb
Uzair Suria <uzair.a.suria@outlook.com>
2020-12-30 14:30:02 +0500

    initalized the project with basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { QuestionCard } from './components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FunctionComponent = () => {
+	const startQuiz = async () => {};
+
+	const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+	const nextQuestion = () => {};
+
+	return (
+		<div className="App">
+			<h1>REACT QUIZ</h1>
+			<button className="start" onClick={startQuiz}>
+				START
+			</button>
+			<p className="score">Score:</p>
+			<p>Loading Questions...</p>
+			<QuestionCard />
+		</div>
+	);
+};
 
 export default App;
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const QuestionCard: React.FunctionComponent = () => {
+	return <div>QuestionCard</div>;
+};
+
+export default QuestionCard;
